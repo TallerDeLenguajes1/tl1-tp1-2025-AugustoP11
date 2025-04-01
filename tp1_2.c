322b5f0ae9e0d0b39407b73a08d1daa7df88089e
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2025-04-01 20:50:40 -0300

    Funcion que devuelve el cuadrado de un numero

---
+++ tp1_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+//Declaracion de funciones
+float numeroAlCuadrado(float numero);
+
+
+
+//funcion main
+int main() {
+
+
+
+    getchar();
+    getchar();
+    return 0;
+}
+
+//Definicion de funciones
+float numeroAlCuadrado(float numero){
+    float cuadrado;
+    cuadrado = numero * numero;
+    return (cuadrado);
+}
+
