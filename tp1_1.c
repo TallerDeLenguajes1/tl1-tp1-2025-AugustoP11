0f9341e6647eaa6d9f4dc391a7a640da37ec153e
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2025-04-01 20:09:59 -0300

    Muestro el contenido del puntero

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,19 @@
 #include<stdio.h>
 
 int main() {
-    printf("hola mundo");
+    //Declaro una variable de tipo entero
+    int numeroA = 10;
+
+    //Declaro un puntero de tipo entero
+    int *punteroA;
+
+    //Hago que el puntero apunte a la variable
+    punteroA = &numeroA;
+
+    //Muestro el contenido del puntero
+    printf("\nEl contenido del puntero es: %d\n", punteroA);
+
+    printf("\nhola mundo");
 
     getchar();
     getchar();
